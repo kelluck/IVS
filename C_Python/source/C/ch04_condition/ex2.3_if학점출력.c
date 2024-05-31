6a9b99c50ccc6942977fc126bc2ba3e93e8a8a07
kelluck20 <kelluck20@gmail.com>
2024-05-31 17:01:10 +0900

    update

--- C_Python/source/C/ch04_condition/ex2.3_if학점출력.c
+++ C_Python/source/C/ch04_condition/ex2.3_if학점출력.c
@@ -7,9 +7,9 @@ int main()
     scanf("%d", &score);
 
     if(90 <= score && score <= 100) printf("A학점\n");
-    else if(score >= 80) printf("B학점\n");
-    else if(score >= 70) printf("C학점\n");
-    else if(score >= 60) printf("D학점\n");
-    else if(score >= 0) printf("F학점\n");
+    else if(score >= 80 && score < 90) printf("B학점\n");
+    else if(score >= 70 && score < 80) printf("C학점\n");
+    else if(score >= 60 && score < 70) printf("D학점\n");
+    else if(score >= 0 && score < 60) printf("F학점\n");
     else printf("유효하지 않은 점수입니다\n");
 }
